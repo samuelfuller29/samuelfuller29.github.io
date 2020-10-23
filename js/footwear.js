d9ca9e506c86c7c51550f5c3dc4f09a96d9d9a98
samuelfuller29.github.io <ful20016@byui.edu>
2020-10-22 19:44:16 -0700

    change

--- js/footwear.js
+++ js/footwear.js
@@ -1,19 +1,17 @@
-function footWear() {
+//INPUT:  Get the weather from the user.
+let weather = document.getElementById("weather").value;
+//PROCESSING: Select the correct input
+let shoes;
+if (weather == "Hot") {
+    shoes = "sandals"
+} else if (weather == "Snow") {
+    shoes = "boots"
+} 
+else if (weather == "Uknown") {
+    shoes = "Unknown"
+}
+let output = shoes;
 
-    //INPUT:  Get the weather from the user.
-    let weather = document.getElementById("weather").value;
-    //PROCESSING: Select the correct input
-    let shoes;
-    if (weather == "Hot") {
-        shoes = "Sandals"
-    } else if (weather == "Snow") {
-        shoes = "boots"
-    } else if (weather == "Rain") {
-        shoes = "galoshes"
-    } else if (weather == "Unknown") {
-        shoes = "The weather today is going to be unpredictable better be ready for anything!" {
-    let output = shoes;
-
-    //Output: Display the message to the user
-    document.getElementById("output").innerHTML = shoes;
+//Output: Display the message to the user
+document.getElementById("output").innerHTML = shoes;
 }
