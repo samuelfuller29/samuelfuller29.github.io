a35c317b755158ddd71554b536a1f229e31f3d46
samuelfuller29.github.io <ful20016@byui.edu>
2020-10-22 19:53:41 -0700

    changes to js

---
+++ js/footwears.js
@@ -0,0 +1,20 @@
+function footWear() {
+
+    //INPUT:  Read the weather from the user.
+    let weather = document.getElementById("weather").value;
+    //PROCESSING: Select the correct input
+    let shoes;
+    if (weather == "Hot") {
+        shoes = "sandals"
+    } else if (weather == "Snow") {
+        shoes = "boots"
+    } else if (weather == "Rain") {
+        shoes = "galoshes"
+    } else if (weather == "Uknown") {
+        shoes = "unknown"
+    }
+    let output = shoes;
+
+    //Output: Display the message to the user
+    document.getElementById("output").innerHTML = shoes;
+}
