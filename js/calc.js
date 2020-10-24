dc4a1e375d6aedcaadd9fe9c8a6030ab748be671
samuelfuller29.github.io <ful20016@byui.edu>
2020-10-24 08:59:30 -0700

    CHANGE

--- js/calc.js
+++ js/calc.js
@@ -5,7 +5,7 @@ function calculator() {
 
     let num1 = parseFloat(document.getElementById('number1').value);
     let num2= parseFloat(document.getElementById('number2').value);
-    let num3 = parseFloat(document.getElementById('myanswer').value);
+    let num3 = parseFloat(document.getElementById('myAnswer').value);
     let operator = (document.getElementById('operator').value);
     //Processing:
     //compute the problem
