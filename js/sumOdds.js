3636d12196716021e54ab482eb2be8124d61af0e
samuelfuller29.github.io <ful20016@byui.edu>
2020-11-06 15:01:59 -0800

    change

--- js/sumOdds.js
+++ js/sumOdds.js
@@ -1,17 +1,20 @@
 function sumOdds() {
-    //INPUT: Take an number input from the user,"number1", to select "sumTotal", declare and set the value of the 
-    //variable "Sumtotal" with "number1".
-    //Declare "number" which is the number that counts from 1 to the 
-    //Declare the variable "sumOdd" which will be the final answer.
-    let endingNumber = document.getElementById("Number1").value;
+    /*INPUT: Take a number input from the user,"userInput", to select "sumOddsTotal", 
+    declare and set the value of the variable "SumOddstotal" with "number1".
+    Declare "number" which will start with 1 and add all odd numbers included within 
+    the number from the user input to declare the variable "sumOdd" 
+    which will be the final answer.*/
+
+    let sumOddsTotal = document.getElementById("userInput").value;
     let number = 1;
     let sumOdd = 0;
-    //PROCESSING: Use a while loop to iterate from "number" to "endingNumber" and add each odd number to "sumOdd".
-    while (number <= endingNumber) {
+
+    //PROCESSING: Use a while loop to iterate from "number" to "sumOddsTotal" and add each odd number to "sumOdd".
+    while (number <= sumOddsTotal) {
         if (number %2 != 0) {
             sumOdd = sumOdd + number;
         }
-        number += 1;
+        number++;
     }
 
 
