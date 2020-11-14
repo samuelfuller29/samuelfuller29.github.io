e3db6525592aacd587f9d1f1431c957f5cd971ea
samuelfuller29.github.io <ful20016@byui.edu>
2020-11-14 10:06:02 -0800

    change

--- js/fiveTimes.js
+++ js/fiveTimes.js
@@ -1,13 +1,14 @@
 function fiveTimes() {
     //INPUT
      // There isn't an input 
+    
     //PROCESSING
-    //Loop 12 times 
+    //Loop through table 12 times 
     let output = "";
     for (let i = 1; i <= 12; i++) {
         output += "5 x " + i + " = " + i * 5 + "<br>";
     }
     //OUTPUT
-    //Outputs the times table
+    //Outputs the times table from 1 to 12 each mutiplied by 5
     document.getElementById("output").innerHTML = output;
 }
