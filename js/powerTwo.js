cdb1032ea2bba2acbffecc06271b7a048a932b34
samuelfuller29.github.io <ful20016@byui.edu>
2020-11-15 18:12:20 -0800

    change

--- js/powerTwo.js
+++ js/powerTwo.js
@@ -1,13 +1,14 @@
 /*INPUT: None
- *PROCESSING: Interate from 0 to 31 and calculate the successive powers of 2
- *OUITPUT: S string of the power resutts seperated by a comma with the last result not followed by a comma
- */
-function powerTwo() {
-    //Initialize variables
+ *PROCESSING: iterate from 0 to 31 and calculate the inclusive powers of 2
+ *OUTPUT: string of the power results seperated by a comma with the last result 
+ not followed by a comma*/
+
+ function powerTwo() {
+    //establish variables
     let powerTwo = "";
     let powerOut = "";
     let p = 0;
-    //Calculate the powers of 2 from 0 through 31
+    //Calculate the powers of 2 from 0 through 31 inclusive
     for (p = 0; p <= 31; p++) {
         powerTwo = Math.pow(2, p);
         if (p < 31) {
